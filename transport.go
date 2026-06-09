0a7b3cab8ce0edb4680c59c1f157a033a580a1c9
Slzdude <sk4ct0r@gmail.com>
2026-06-09 20:12:12 +0800

    chore: bump sdkVersion to 0.2.92

--- transport.go
+++ transport.go
@@ -23,7 +23,7 @@ import (
 const (
 	defaultMaxBufferSize     = 1024 * 1024
 	minimumClaudeCodeVersion = "2.0.0"
-	sdkVersion               = "0.2.91"
+	sdkVersion               = "0.2.92"
 )
 
 // Track live CLI subprocesses so we can terminate them when the parent process
